725093bedcdfc596f0eadcdde635f54b1fa94058
Vignesh <vraja@gsu.edu>
2020-11-20 16:18:49 -0500

    Add plugin boiler plate source

---
+++ dfss-ohack/includes/class-dfss-ohack-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.ohack.org/
+ * @since      1.0.0
+ *
+ * @package    Dfss_Ohack
+ * @subpackage Dfss_Ohack/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Dfss_Ohack
+ * @subpackage Dfss_Ohack/includes
+ * @author     OHACK#TEAM13 <sanjose@dressforsuccess.org>
+ */
+class Dfss_Ohack_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ dfss-ohack/public/class-dfss-ohack-public.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://www.ohack.org/
+ * @since      1.0.0
+ *
+ * @package    Dfss_Ohack
+ * @subpackage Dfss_Ohack/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Dfss_Ohack
+ * @subpackage Dfss_Ohack/public
+ * @author     OHACK#TEAM13 <sanjose@dressforsuccess.org>
+ */
+class Dfss_Ohack_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dfss_Ohack_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dfss_Ohack_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dfss-ohack-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dfss_Ohack_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dfss_Ohack_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dfss-ohack-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
